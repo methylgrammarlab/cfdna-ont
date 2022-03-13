28df7618bb4cf221f802b5d8a25abf837fb6e58b
ekushele <56130301+ekushele@users.noreply.github.com>
2022-03-13 12:20:00 +0200

    Add files via upload
    after updating suffix and arguments

--- deconvolution_code/TCGA_probes/plot_score_of_methylation_according_to_nanopore_sampeles_create_table.R
+++ deconvolution_code/TCGA_probes/plot_score_of_methylation_according_to_nanopore_sampeles_create_table.R
@@ -1,17 +1,18 @@
-source("/cs/icore/ekushele/script/r/filippo/functions_atlas.R")
-output_dir="/vol/sci/bio/data/benjamin.berman/ekushele/filippo/data/new_atlas/results/"
+source("/sci/labs/bermanb/ekushele/icore-home/script/r/filippo/functions_atlas.R")
+output_dir="/sci/labs/bermanb/ekushele/icore-data/filippo/data/new_atlas/results/"
 
 #create methylation score table
 pvalue=0.001
 print(pvalue)
 diff_mean=0.3
+args = commandArgs(trailingOnly=TRUE)
 
-
+ 
 #get cpgs unique to LUAD-get normal lung CpGs
-num_cpg <- 2000
+num_cpg <- as.numeric(args[4]) 
 print(paste("Number initial CpGs in atlas:",num_cpg))
 #get josh data
-best_hypo_hyper_probes_file <- paste0( "/vol/sci/bio/data/benjamin.berman/ekushele/filippo/data/josh_moss_probes/creating_new_atlas_scripts/CpGs.100bp-block.",num_cpg,".X1.xls")
+best_hypo_hyper_probes_file <- paste0( "/sci/labs/bermanb/ekushele/icore-data/filippo/data/josh_moss_probes/creating_new_atlas_scripts/CpGs.100bp-block.",num_cpg,".X1.xls")
 best_hypo_hyper_probes <- data.frame(fread(best_hypo_hyper_probes_file))[,c("acc","group","name")]
 best_hypo_hyper_probes$name <- gsub(" ","_",best_hypo_hyper_probes$name)
 best_probes_tissue <-subset(best_hypo_hyper_probes,name=="Lung_cells")
@@ -27,21 +28,27 @@ data_on_files <- function(cur_file,sample_mapped,sample_name,tissue_file) {
 	cur_file <-cur_file[cur_file$FDR_adjust < pvalue & abs(cur_file$mean_diff) > diff_mean & !is.na(cur_file$FDR_adjust), ]
 	if (nrow(cur_file)>0) {
 		CpGs <- cur_file$CpGs_common
-		#CpGs <- setdiff(CpGs,best_hypo_hyper_probes[best_hypo_hyper_probes$name=="Lung_cells","acc"])
+		CpGs <- setdiff(CpGs,best_hypo_hyper_probes[best_hypo_hyper_probes$name=="Lung_cells","acc"])
 
 		sample_mapped$cpg_index <- sub("^","cg",sample_mapped$cpg_index)
 
-		sample_overlap_meth <- sample_mapped[which(sample_mapped$cpg_index %in% intersect(sample_mapped$cpg_index,CpGs)),c("cpg_index","V5","V6","V7", "methylation")]
+		# sample_overlap_meth <- sample_mapped[which(sample_mapped$cpg_index %in% intersect(sample_mapped$cpg_index,CpGs)),c("cpg_index","V5","V6","V7", "methylation")]
+		sample_overlap_meth_coordinates <- sample_mapped[which(sample_mapped$cpg_index %in% intersect(sample_mapped$cpg_index,CpGs)),c("cpg_index","V5","V6","V7", "methylation")]
+		sample_overlap_meth <- aggregate(methylation~cpg_index,data=sample_overlap_meth_coordinates,FUN=mean, na.rm=TRUE, na.action=na.pass)
+	
 		#CpGs includes CpGs that repeats themselved in sample_mapped
+		#sample_overlap_meth <- aggregate(.~cpg_index,data=sample_overlap_meth[,-c(2,3,4)],FUN=mean, na.rm=TRUE, na.action=na.pass) 
+
 		CpGs = sample_overlap_meth$cpg_index
 		
-		CpGs_numbers_rep <- plyr::count(CpGs)
-		CpGs_numbers_rep <- CpGs_numbers_rep[match(unique(CpGs),CpGs_numbers_rep$x),]
-		cur_file <-cur_file[which(cur_file$CpGs_common %in% intersect(CpGs,cur_file$CpGs_common)),]
-
+		# CpGs_numbers_rep <- plyr::count(CpGs)
+		# CpGs_numbers_rep <- CpGs_numbers_rep[match(unique(CpGs),CpGs_numbers_rep$x),]
+		cur_file <- cur_file[which(cur_file$CpGs_common %in% intersect(CpGs,cur_file$CpGs_common)),]
 	 	cur_file <- cur_file[match(unique(CpGs),cur_file$CpGs_common),]
-	 	cur_file <- cur_file[rep(1:nrow(cur_file), times=CpGs_numbers_rep$freq),]
+	 	#cur_file <- cur_file[rep(1:nrow(cur_file), times=CpGs_numbers_rep$freq),]
 		# sample_overlap_meth <- sample_overlap_meth[match(cur_file$CpGs_common,sample_overlap_meth$cpg_index),]
+		sample_overlap_meth <- merge(sample_overlap_meth,aggregate(.~cpg_index,sample_overlap_meth_coordinates[,-c(5)],head,1),by="cpg_index")
+
 		score_of_cpg <- (sample_overlap_meth$methylation - cur_file[,"mean_healthy"])/(cur_file$mean_tumor-cur_file$mean_healthy)
 		nnls_score_of_cpg <- nnls::nnls(as.matrix(data.frame(cur_file$mean_tumor,cur_file$mean_healthy)),sample_overlap_meth$methylation)$x
 	}
@@ -56,33 +63,39 @@ data_on_files <- function(cur_file,sample_mapped,sample_name,tissue_file) {
 
 #find the hypo-hyper methylation for each sample
 all_t_test_files <- dir(file.path(output_dir,"atlas_vs_healthy","t_test"),pattern="csv",full.names = T)
-all_corrected_TCGA_files <- dir("/vol/sci/bio/data/benjamin.berman/ekushele/filippo/data/new_atlas/TCGA_purity/",pattern="csv",full.names = T)
-samples_mapped_to_CpG <- dir(file.path("/vol/sci/bio/data/benjamin.berman/ekushele/filippo/data/samples_nanopore/","samples_merged_CpG_450K"),full.names=T,pattern="_shifted_mapped_CpG.bed.gz")
-
+all_corrected_TCGA_files <- dir("/sci/labs/bermanb/ekushele/icore-data/filippo/data/new_atlas/TCGA_purity/",pattern=".csv",full.names = T)
+#samples_mapped_to_CpG <- dir(file.path("/sci/labs/bermanb/ekushele/icore-data/filippo/data/samples_nanopore/","samples_merged_CpG_450K"),full.names=T,pattern="_shifted_mapped_CpG.bed.gz")
+# samples_mapped_to_CpG <- dir(file.path("/sci/labs/bermanb/ekushele/icore-data/filippo/data/samples_nanopore_020222/","samples_merged_CpG_450K_links_060222"),full.names=T,pattern="_shifted_mapped_CpG.bed.gz")
+samples_mapped_to_CpG <- dir(args[1],full.names=T,pattern="_shifted_mapped_CpG.bed.gz")
+ 
 all_table_methylation_in_tissue <- do.call(rbind,lapply(samples_mapped_to_CpG,function(sample_bed) {
 	print(sample_bed)
 	sample_mapped <- data.frame(fread(sample_bed,colClasses = c('V4'='character')))
 	colnames(sample_mapped)[4] <- "cpg_index"
 	colnames(sample_mapped)[8] <- "methylation"
 	#take only sample_mapped where there are CpGs with reasonable	 FDR exists
-	do.call(rbind,parallel::mclapply(all_t_test_files,function(tissue_file) {
+	do.call(rbind,parallel::mclapply(all_t_test_files[17],function(tissue_file) {
 		print(tissue_file)
 		cur_file <- data.frame(fread(tissue_file))
 		if (nrow(cur_file)>0)  {
 			cur_corrected_TCGA_file <- grep(basename(tissue_file),all_corrected_TCGA_files,value=T)
 			cur_corrected_TCGA_file <- colMeans(data.frame(fread(cur_corrected_TCGA_file)),na.rm=T)
 			cur_corrected_TCGA_file <- data.frame(pure_cpg =cur_corrected_TCGA_file) 
+###TODO
+
+			# cur_corrected_TCGA_file <- data.frame(fread(grep(sub(".csv",basename(tissue_file),all_corrected_TCGA_files,value=T)))
+			
 			cur_file <- merge(cur_file,cur_corrected_TCGA_file,by.x="CpGs_common",by.y="row.names")
 			cur_file$mean_tumor <- cur_file$pure_cpg
 		}
 			scores_of_meth <- data_on_files(cur_file,sample_mapped,sub("\\..*","",basename(sample_bed)),tissue_file)
-		data.frame(cancer_type=sub("\\.csv","",basename(tissue_file)),sample=basename(sample_bed),scores_of_meth)
+		data.frame(cancer_type=sub("\\.csv","",basename(tissue_file)),sample=sub("\\.hg38.*","",basename(sample_bed)),scores_of_meth)
 	},mc.cores=future::availableCores()[[1]]))
 }))
 
 ####====scoring
-samples_healthy <- c("BC02","BC03","BC05","BC04","BC12")
- 
+# samples_healthy <- c("BC02","BC03","BC05","BC04","BC12","HU005.10","HU005.11","HU005.12")
+samples_healthy <- unlist(strsplit(args[3],split=",")) 
 
 all_table_methylation_in_tissue[is.na(all_table_methylation_in_tissue)] <- 0
 all_table_methylation_in_tissue$category <- "Tumor"
@@ -102,8 +115,14 @@ all_table_methylation_in_tissue <- cbind(all_table_methylation_in_tissue[,grep("
 # file_to_write <- "methylation_in_sample_per_tissue_score_table_all_samples_new_score.csv"
 #file_to_write <- "methylation_in_sample_LUAD_score_table_all_samples_new_score_tumor_only.csv"
 # file_to_write <- "methylation_in_sample_LUAD_score_table_all_samples_new_score_tumor_only_unique_to_LUAD.csv"
-file_to_write <- "methylation_in_sample_per_tissue_table_all_samples_new_score_tumor_only.csv"
-fwrite(all_table_methylation_in_tissue,file.path(output_dir,"atlas_vs_healthy",file_to_write))
-file_name_to_link <- "methylation_in_sample_per_tissue_score_table_all_samples_all_normalization.csv"
+# file_to_write <- "methylation_in_sample_LUAD_score_table_all_samples_new_healthy_new_score_tumor_only_unique_to_LUAD.csv" 
+# file_to_write <- "methylation_in_sample_LUAD_score_table_all_samples_remora1_060222_new_score_tumor_only_unique_to_LUAD.csv"  
+# file_to_write <- "methylation_in_sample_LUAD_score_table_all_samples_remora1_080222_new_score_tumor_only_unique_to_LUAD.csv"  
+file_to_write <- paste0("methylation_in_sample_LUAD_new_score_tumor_only_unique_to_LUAD",args[2],".csv" )
+# file_to_write <- "methylation_in_sample_per_tissue_table_all_samples_new_score_tumor_only.csv"
 
+file_to_write_path <- file.path(output_dir,"atlas_vs_healthy",file_to_write)
+fwrite(all_table_methylation_in_tissue,file_to_write_path)
+file_name_to_link <- "methylation_in_sample_per_tissue_score_table_all_samples_all_normalization.csv"
 system(paste("ln -sf",file.path(output_dir,"atlas_vs_healthy",file_to_write),file.path(output_dir,"atlas_vs_healthy",file_name_to_link)))
+message(paste("The file",file_to_write_path,"was written"))
